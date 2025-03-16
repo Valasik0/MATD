9e0e5c4694be1c311cac68a9a64c5e75426d0771
Vaclav Vyrobik <vaclav.vyrobik@gmail.com>
2025-03-16 19:04:15 +0100

    create matd repo

---
+++ cv2/main.py
@@ -0,0 +1,15 @@
+def broot_force_search(arr, target):
+    positions = []
+    for i in range(len(arr) - len(target) + 1):
+        for j in range(len(target)):
+            if arr[i + j] != target[j]:
+                break
+        else:
+            positions.append(i)
+
+    return positions
+
+text = "ABCDABCDAB"
+target = "AB"
+
+print(broot_force_search(text, target))
