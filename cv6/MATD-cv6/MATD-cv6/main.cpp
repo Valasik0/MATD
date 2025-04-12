e6792c6c51912bcdc6bacf684975eef40e896b1c
Vaclav Vyrobik <vaclav.vyrobik@gmail.com>
2025-04-12 21:31:47 +0200

    add Elias gamma decoder

--- cv6/MATD-cv6/MATD-cv6/main.cpp
+++ cv6/MATD-cv6/MATD-cv6/main.cpp
@@ -5,12 +5,12 @@
 #include "EliasGammaEncoder.h"
 
 int main() {
-	std::vector<uint32_t> nums = {1, 2, 3, 4, 5};
-	std::vector<bool> sequence = {0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1};
+	std::vector<uint32_t> nums = { 1, 2, 3, 4, 5 };
+	std::vector<bool> sequence = { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
 	UnaryEncoder ue = UnaryEncoder(nums);
 	ue.encode();
 	ue.printEncoded();
-	
+
 	std::cout << "Original size in bits: " << ue.getOriginalSizeBits() << std::endl;
 	std::cout << "Encoded size in bits: " << ue.getEncodedSizeBits() << std::endl;
 
@@ -18,11 +18,18 @@ int main() {
 	for (uint32_t val : ue.decode(sequence))
 		std::cout << val;
 
-	std::cout << "------------------------------" << std::endl;
+	std::cout << std::endl << "------------------------------" << std::endl;
 
 	EliasGammaEncoder eg = EliasGammaEncoder(nums);
-	eg.encode();
+	std::vector<bool> encoded = eg.encode();
+	eg.printEncoded();
 
+	std::cout << "Original size in bits: " << eg.getOriginalSizeBits() << std::endl;
+	std::cout << "Encoded size in bits: " << eg.getEncodedSizeBits() << std::endl;
+
+	std::cout << "Decoded sequence: ";
+	for (uint32_t val : eg.decode(encoded))
+		std::cout << val;
 
 	return 0;
 }
