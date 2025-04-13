888f443665717a855d0703c97f6cc64e697db3ed
Vaclav Vyrobik <vaclav.vyrobik@gmail.com>
2025-04-13 10:42:52 +0200

    Refactor inheritance

---
+++ cv6/MATD-cv6/MATD-cv6/Encoder.cpp
@@ -0,0 +1,20 @@
+#include "Encoder.h"
+#include <iostream>
+
+Encoder::Encoder(const std::vector<uint32_t>& nums) {
+    this->nums = nums;
+}
+
+void Encoder::printEncoded() {
+    for (bool bit : encodedBits)
+        std::cout << bit;
+    std::cout << std::endl;
+}
+
+size_t Encoder::getEncodedSizeBits() {
+    return encodedBits.size();
+}
+
+size_t Encoder::getOriginalSizeBits() {
+    return sizeof(uint32_t) * nums.size() * 8;
+}
