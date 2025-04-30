5ddcbf21ff4a1398313e89fd79efb8d513df4924
Vaclav Vyrobik <vaclav.vyrobik@gmail.com>
2025-04-30 17:43:31 +0200

    Add type hints to functions and improve context-target pair creation; implement softmax and cross-entropy loss functions

--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 import re
 from collections import Counter
 import numpy as np
+from typing import List, Tuple, Dict
 
 def open_and_read_file(file_path: str) -> str:
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -10,7 +11,7 @@ def open_and_read_file(file_path: str) -> str:
             text = text.replace(c, "")
     return text
 
-def tokenize(text: str) -> tuple:
+def tokenize(text: str) -> Tuple[List[int], Dict[str, int]]:
     splitted = re.findall(r'\w+|[^\w\s]', text.lower(), re.UNICODE)
 
     vocab_size = 5000
@@ -21,7 +22,7 @@ def tokenize(text: str) -> tuple:
 
     return indexed_tokens, vocab
 
-def create_context_target_pairs(indexed_tokens: list, window_size: int = 2) -> tuple:
+def create_context_target_pairs(indexed_tokens: List[int], window_size: int = 2) -> Tuple[List[List[int]], List[int]]:
     X = []
     y = []
 
@@ -33,12 +34,39 @@ def create_context_target_pairs(indexed_tokens: list, window_size: int = 2) -> t
 
     return X, y
 
+def softmax(x): #prevede z (z metodz cbow_forward) na pravděpodobnosti
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
 
-def cbow_forward(context, E, W, b):
-    context_embeddings = E[context]  # shape: (context_size, embedding_dim)
-    h = np.mean(context_embeddings, axis=0)  # shape: (embedding_dim,)
-    u = np.dot(h, W) + b  # shape: (vocab_size,)
-    return u 
+def cross_entropy_loss(y_pred, y_true):
+    return -np.log(y_pred[y_true] + 1e-10)
+
+def cbow_forward(context: List[int], E: np.ndarray, W: np.ndarray, b: np.ndarray) -> np.ndarray:
+    context_embeddings = E[context]
+    h = np.mean(context_embeddings, axis=0)
+    z = np.dot(h, W) + b #z = W * V_context + b, shape (vocab_size), logits
+    return z 
+
+def cbow_backpropagation(context, target, y_pred, E, W, b, learning_rate):
+    vocab_size = y_pred.shape[0]
+    context_size = len(context)
+    h = np.mean(E[context], axis=0)
+    
+    y_true = np.zeros(vocab_size)
+    y_true[target] = 1
+    
+    delta = y_pred - y_true
+    
+    # 3. Gradienty
+    dW = np.outer(h, delta)  # (embedding_dim, vocab_size)
+    db = delta
+    dh = np.dot(W, delta)  # (embedding_dim,)
+    
+    # 4. Update váhy
+    W -= learning_rate * dW
+    b -= learning_rate * db
+    for idx in context:
+        E[idx] -= learning_rate * (dh / context_size)
 
 
 text = open_and_read_file('czech_news_content.txt')
@@ -52,4 +80,7 @@ E = np.random.normal(0, 0.1, (vocab_size, embedding_dim))
 W = np.random.normal(0, 0.1, (embedding_dim, vocab_size))
 b = np.zeros(vocab_size)
 
-print(E[X[0]])
+z = cbow_forward(X[0], E, W, b)
+
+print(softmax(z))
+
